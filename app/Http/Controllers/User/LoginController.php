dfb2bb7916cbd8c151e5eb141ad3855011084599
Gurwinder Singh <SinghGurwinder@SEASIAINFOTECH.COM>
2023-02-27 16:03:20 +0530

    ticket-164 fix user login api

--- app/Http/Controllers/User/LoginController.php
+++ app/Http/Controllers/User/LoginController.php
@@ -58,13 +58,13 @@ class LoginController extends Controller
                
                 if($user->is_active && $user->email_verified_at != null){
                     $success['user']  = $user;
-                    if($user->user_type == User::TEACHER_TYPE && $request->user_type != User::TEACHER_TYPE){
-                        return sendError('User is Not a Teacher',[], 403);
-                    }
-                    if($user->user_type == User::STUDENT_TYPE && $request->user_type != User::STUDENT_TYPE){
+                    if($user->user_type == User::TEACHER_TYPE && $request->user_type != User::STUDENT_TYPE){
                         return sendError('User is Not a Student',[], 403);
                     }
-                    
+                    if($user->user_type == User::STUDENT_TYPE && $request->user_type != User::TEACHER_TYPE){
+                        return sendError('User is Not a Teacher',[], 403);
+                    }
+
                     if ($user->user_type == User::TEACHER_TYPE) {
                         if($user->teacher_status == User::TEACHER_STATUS_PENDING){
                             $data['profile_status']  = 'pending';
